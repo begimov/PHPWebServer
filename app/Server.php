ffd1793c478e1152edc94a6b10529df253bfdba9
begimov <begimov@gmail.com>
2018-05-26 12:25:50 +0300

    Server's bind method to bind socket to a host/port has been implemented

--- app/Server.php
+++ app/Server.php
@@ -14,4 +14,11 @@ class Server
     {
         $this->socket = socket_create(AF_INET, SOCK_STREAM, 0);
     }
+
+    protected function bind()  
+    {
+        if (!socket_bind( $this->socket, $this->host, $this->port)) {
+            throw new Exception('Could not bind: '.$this->host.':'.$this->port.' - '.socket_strerror(socket_last_error()));
+        }
+    }
 }
