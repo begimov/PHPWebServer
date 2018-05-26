797560bb823538fc470b5d93e9e4ed21a0322d5a
begimov <begimov@gmail.com>
2018-05-26 12:46:42 +0300

    Server's listen method has been implemented

--- app/Server.php
+++ app/Server.php
@@ -31,4 +31,38 @@ class Server
             throw new Exception('Could not bind: '.$this->host.':'.$this->port.' - '.socket_strerror(socket_last_error()));
         }
     }
+
+    public function listen($callback)  
+    {
+        if (!is_callable( $callback ))
+        {
+            throw new Exception('The given argument should be callable.');
+        }
+
+        while (1) 
+        {
+            socket_listen($this->socket);
+
+            if (!$client = socket_accept($this->socket)) 
+            {
+                socket_close($client);
+                continue;
+            }
+
+            $request = Request::withHeaderString(socket_read($client, 1024));
+
+            $response = call_user_func($callback, $request);
+
+            if (!$response || !$response instanceof Response)
+            {
+                $response = Response::error( 404 );
+            }
+
+            $response = (string) $response;
+
+            socket_write($client, $response, strlen($response));
+
+            socket_close($client);
+        }
+    }
 }
